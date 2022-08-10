13cb07d38db1574e085409a80993c1b1e2cf252a
Praneeth musunuri <praneethmusunuri9@gmail.com>
2022-08-10 15:42:41 -0500

    comitted
    comitted

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import App from "./App";
+import "./index.css";
+
+const client = new QueryClient();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <QueryClientProvider client={client}>
+    <App />
+  </QueryClientProvider>
+);
